4fdd5c0d19f6cdea552b1c7e7336b2f6856c3fe8
maxwell.hatfield <maxwell.hatfield@MU-C1-18.mullen.pvt.k12.co.us>
2021-10-26 11:10:05 -0600

    initial

---
+++ ccard/src/ccard/creditVal.java
@@ -0,0 +1,83 @@
+package ccard;
+
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+public class creditVal
+	{
+	static long  num[]  = new long[16];
+	static int valid =0;
+	static int ivalid=0;
+
+		public static void main(String[]args) throws IOException
+		{
+			
+getNum();
+	
+		
+			}
+		public static void getNum() throws IOException
+		{
+			System.out.println("d");
+			Scanner myFile = new Scanner( new File ("creditnums.txt"));
+			while(myFile.hasNextLong());
+				{
+					long number = myFile.nextLong();
+					for(int i = 15; i>= 0; i--)
+						{
+
+							num[i]= number%10;
+							number = number/10;
+							
+						}
+					changeNumber();
+					checkReal();
+				}
+			
+			
+		}
+		public static void changeNumber()
+		{
+			for(int i =0; i < num.length; i=+2)
+			{
+				num[i]=num[i]*2;
+				if(num[i]>9)
+				{
+					long one = num[i]%10;
+					long two = num[i]/10;
+					num[i]=one+two;
+				}
+			}
+		}
+		public static void checkReal()
+		{
+			int fin =0;
+			for(int i =0; i<num.length; i++)
+			{
+				fin +=  num[i];
+			}
+			if(fin%10 == 0)
+			{
+				valid++;
+				for(int i = 0; i<16; i++)
+				{
+					System.out.println(num[i]);
+				}
+			}
+			else
+			{
+				ivalid++;
+			}
+		}
+}
+		
+		
+
+	
+
+
+
+
+
+
